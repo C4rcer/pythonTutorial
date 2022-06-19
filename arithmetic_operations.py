6e19b2e0edb5cd1119f29dae521de1d0c4682040
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-19 12:56:43 +0100

    Completed lesson

---
+++ arithmetic_operations.py
@@ -0,0 +1,14 @@
+print(10 + 3)  # Addition
+print(10 - 3)  # Subtraction
+print(10 * 3)  # Multiplication
+print(10 / 3)  # Division (float)
+print(10 // 3)  # Division (drops the floating point)
+print(10 % 3)  # Modulo (shows the remainder of the division)
+print(10 ** 3)  # Exponent (the power)
+
+x = 10
+x = x + 3  # increments number
+print(x)
+
+x += 3  # Augmented assignment operator subtract works too.
+
