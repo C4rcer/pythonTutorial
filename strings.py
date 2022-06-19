fe06df75eb3d690b9a1db851bc04f752bf6cae40
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-19 09:25:27 +0100

    Completed Lesson

---
+++ strings.py
@@ -0,0 +1,4 @@
+course = 'Python for Beginners'
+print(course[0:])  # start index of 0 and undefined end index (will complete full string)
+print(course[0:3])  # start index of 0 and end index of 3 will complete to Pyt
+print(course[:3])  # No start index will assume 0 and end at 3 Pyt again.
--- type_conversion_exercise.py
+++ type_conversion_exercise.py
@@ -3,5 +3,3 @@ print(type(weight_lbs))
 weight_kg = int(weight_lbs) * 0.45
 print(type(weight_kg))
 print(weight_kg)
-
-
