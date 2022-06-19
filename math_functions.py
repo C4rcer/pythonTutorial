a7e719cac700c3eb54b7a5896a4d6a3cc425fd1b
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-19 16:31:50 +0100

    Completed exercise

---
+++ math_functions.py
@@ -0,0 +1,7 @@
+import math  # object for math containing reusable functions
+
+print(math.ceil(2.9))  # ceiling rounds up
+print(math.floor(2.9))  # floor rounds down google mathematical functions for more functions in python.
+x = 2.9
+print(round(x))  # rounds the value to the nearest whole
+print(abs(-2.1))  # absolute always returns the positive representation of a value
