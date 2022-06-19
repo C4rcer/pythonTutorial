31d32424580dec32a50550718544ca1f37702fc8
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-19 11:06:13 +0100

    Completed lesson

---
+++ string_methods_lesson.py
@@ -0,0 +1,8 @@
+course = 'Python for Beginners'
+print(course.upper())  # method to change a string to uppercase
+print(course.lower())
+print(course.find('p'))  # prints the index is case-sensitive
+print(course.replace('Beginners', 'Absolute Beginners'))  # replace method can do single letters
+print('Python' in course)  # boolean expressions to check if a string contains a value
+
+
