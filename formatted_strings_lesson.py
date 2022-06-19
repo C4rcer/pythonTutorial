55a36709cc39bb0adc5c5c800d1598ce52521806
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-19 10:30:23 +0100

    Completed lesson

---
+++ formatted_strings_lesson.py
@@ -0,0 +1,6 @@
+first = 'John'
+last = 'Smith'
+message = first + ' [' + last + '] is a coder'
+msg = f'{first} [{last}] is a coder'  # formatted string {} being holes in the string we can populate
+print(message)
+print(msg)
