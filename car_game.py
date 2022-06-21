e5fb2f352e8e9c565a6d5f99e46c2fbe06a56d11
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-21 15:17:16 +0100

    Completed lesson

--- car_game.py
+++ car_game.py
@@ -4,10 +4,10 @@ started = False
 while True:
     command = input("> ").lower()
     if command == "start":
-        if started:
+        if started:  # if the boolean value "started" is True this will print
             print("Car is already started!")
         else:
-            started = True
+            started = True  # else it sets the boolean "started" to True and this will print
             print("Car started...")
     elif command == "stop":
         if not started:
