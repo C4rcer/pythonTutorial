5940039ad42be599fa019e60182043da640fd07b
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-21 15:21:43 +0100

    Completed lesson

--- car_game.py
+++ car_game.py
@@ -1,14 +1,14 @@
 command = ""
-started = False
+started = False # simple yes/no true/false boolean that can change to reflect the status of the car
 
 while True:
     command = input("> ").lower()
     if command == "start":
-        if started:  # if the boolean value "started" is True this will print
-            print("Car is already started!")
+        if started:  # if the boolean value "started" is True
+            print("Car is already started!")  #  then this will print
         else:
-            started = True  # else it sets the boolean "started" to True and this will print
-            print("Car started...")
+            started = True  # else it sets the boolean "started" to True
+            print("Car started...")  # then this will print
     elif command == "stop":
         if not started:
             print("Car is already stopped!")
