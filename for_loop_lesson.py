a1c527d345e6533b0288711ec2db6eed3ad70c1e
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-06-25 22:49:09 +0100

    Completed lesson

---
+++ for_loop_lesson.py
@@ -0,0 +1,8 @@
+for item in ["Phil", "John", "Sarah"]:  # loop variable (item) [] list
+    print(item)
+
+for item in range(10):  # range prints 0 - 9 (computers count from zero)
+    print(item)
+
+for item in range(5, 10, 2):  # range between 5 and 10 but in steps of two so 5,7,9 print
+    print(item)
