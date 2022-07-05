93515656054db99dac2c58f678adeec63f489964
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-05 17:24:47 +0100

    Completed exercise

--- list_methods_exercise.py
+++ list_methods_exercise.py
@@ -1,6 +1,6 @@
-numbers = [5, 2, 1, 7, 4, 4, 2, 10]
-uniques = []
-for number in numbers:
-    if number not in uniques:
-        uniques.append(number)
+numbers = [5, 2, 1, 7, 4, 4, 2, 10]  # List
+uniques = []  # New list to store numbers
+for number in numbers:  # for each number in numbers
+    if number not in uniques:  # if number is not already in uniques
+        uniques.append(number)  # Append the number into uniques
 print(uniques)
