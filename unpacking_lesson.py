fcaf8a842ac9f669bf202502a7fce7ebee8a9eae
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-06 18:58:03 +0100

    Completed lesson

---
+++ unpacking_lesson.py
@@ -0,0 +1,6 @@
+coordinates = (1, 2, 3)
+# x = coordinates[0] inefficient way of setting the cords from the tuple
+# y = coordinates[1]
+# z = coordinates[2]
+
+x, y, z = coordinates  # This unpacks the tuple in order to these variables, can be done with lists too
