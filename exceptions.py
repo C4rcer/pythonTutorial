4faf1c25305e9d36d10ca8545fd8316de2eef016
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-07 19:36:24 +0100

    Completed exercise, came close this time to doing it on my own.

---
+++ exceptions.py
@@ -0,0 +1,9 @@
+try:  # Try running this code
+    age = int(input("Age: "))
+    income = 27000
+    risk = income / age
+    print(age)
+except ZeroDivisionError:  # exception if division by zero then print below
+    print("Age cannot be 0.")
+except ValueError:  # instead of crashing print this if it's a value error
+    print("Invalid value")
