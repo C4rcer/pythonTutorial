11b99e605d14127c08b7b652f5ffa5f3a886149b
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-07 16:04:53 +0100

    Completed lesson

---
+++ parameters_lesson.py
@@ -0,0 +1,11 @@
+# function is a container for a few lines of code that do a specific task
+def greet_user(first_name, last_name):  # Meaningful name, name is a parameter
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome aboard")
+
+
+# Need two line breaks
+print("Start")
+greet_user("John", "Smith")  # Function must be defined before calling
+greet_user("Mary", "Poppins")  # Mary is an argument that we pass top the name parameter of the greet_user function
+print("Finish")
