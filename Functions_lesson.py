7a95761a5c103511209315f78a14dd6a6bb9656a
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-07 14:42:50 +0100

    Completed lesson

---
+++ Functions_lesson.py
@@ -0,0 +1,10 @@
+# function is a container for a few lines of code that do a specific task
+def greet_user():  # Meaning full name
+    print("Hi there!")
+    print("Welcome aboard")
+
+
+# Need two line breaks
+print("Start")
+greet_user()  # Function must be defined before calling
+print("Finish")
