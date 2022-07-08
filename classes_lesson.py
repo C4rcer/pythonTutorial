72f574ec49b74b3e8562c30176df5822b7cccd28
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-08 12:25:18 +0100

    Completed lesson

--- classes_lesson.py
+++ classes_lesson.py
@@ -5,20 +5,20 @@
 # ---
 # Lists
 # Dictionaries
-class Point:  # Class are named differently
+class Point:  # Class naming convention is Pascal (Capitalise start of each word)
     def move(self):
         print("move")
 
-    def draw(self):  # template for an object
+    def draw(self):
         print("draw")
 
 
-point1 = Point()  # creates new object and returns, it's now stored in point1
-point1.x = 10
+point1 = Point()  # creates new object which is an instance of a class (blueprint)
+point1.x = 10  # Attribute set
 point1.y = 20
 print(point1.x)
 point1.draw()
 
-point2 = Point()
+point2 = Point()  # New object is not the same as point1 but is still an instance of the point class
 point2.x = 1
 print(point2.x)
