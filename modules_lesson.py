eeb34871d77c01fe88bef5f0275402865dea82d0
C4rcer <107530406+C4rcer@users.noreply.github.com>
2022-07-08 17:32:23 +0100

    Completed lesson

---
+++ modules_lesson.py
@@ -0,0 +1,7 @@
+import converters_modules_lesson  # Import the module with the functions we made
+
+from converters_modules_lesson import kg_to_lbs  # we can call single functions this way
+
+kg_to_lbs(100)  # we do not need to use the .function selection in the 2nd example
+
+print(converters_modules_lesson.kg_to_lbs(70))
